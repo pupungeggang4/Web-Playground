b66b49d2ff88ac497e042a385ddd0b998f22c0bc
pupungeggang6 <pupungeggang6@gmail.com>
2025-08-16 01:11:04 +0900

    Start

--- adventure/game/script/ui.js
+++ adventure/game/script/ui.js
@@ -8,10 +8,15 @@ const UI = {
     },
 
     game: {
-
+        buttonBack: [40, 1160, 80, 80],
     },
 
     menu: {
-
+        rect: [240, 240, 640, 240],
+        textPause: [264, 264],
+        buttonResume: [240, 320, 640, 80],
+        textResume: [264, 344],
+        buttonExit: [240, 400, 640, 80],
+        textExit: [264, 424],
     },
 }
