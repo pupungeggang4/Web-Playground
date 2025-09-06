9aa1faf23d595d614003c05af343b862268dfaaa
pupungeggang2 <kyongkyong2892@gmail.com>
2025-09-07 02:58:21 +0900

    Temp

--- defense/game/script/game.js
+++ defense/game/script/game.js
@@ -4,7 +4,9 @@ class Game {
         this.state = ''
         this.menu = false
 
-
+        this.canvas = document.gerElementById('screen')
+        this.ctx = this.canvas.getContext('2d')
+        this.canvas.addEventListener('mouseup', (event) => this.mouseUp(event), false)
     }
 
     loop() {
