d4324643d0229709b07bc27d71a9265b70c06baf
pupungeggang2 <kyongkyong2892@gmail.com>
2025-10-10 17:20:47 +0900

    Start

--- roguelike/src/script/locale.js
+++ roguelike/src/script/locale.js
@@ -1,11 +1,17 @@
 class Locale {
     static data = {
         'en': {
-            gameName: 'Roguelike'
+            gameName: 'Roguelike',
+            startGame: 'Start Game',
+            collection: 'Collection',
+            eraseData: 'Erase Data'
         },
 
         'ko': {
-            gameName: '로그라이크'
+            gameName: '로그라이크',
+            startGame: '게임 시작',
+            collection: '수집품',
+            eraseData: '데에터 삭제'
         },
     }
 }
--- roguelike/src/script/ui.js
+++ roguelike/src/script/ui.js
@@ -1,5 +1,19 @@
 class UI {
     static title = {
-        textTitle: [24, 24]
+        textTitle: [24, 24],
+        buttonStart: [160, 160, 960, 80],
+        textStart: [184, 184],
+        buttonCollection: [160, 240, 960, 80],
+        textCollection: [184, 264],
+        buttonErase: [160, 320, 960, 80],
+        textErase: [184, 344],
+    }
+
+    static village = {
+        buttonMenu: [1180, 20, 80, 80]
+    }
+
+    static battle = {
+        buttonMenu: [1180, 20, 80, 80]
     }
 }
